4830328bf7a9428e7b025d78178d457546d63748
JakeTheViking <J.n.vanmarle@gmail.com>
2017-03-08 13:31:57 +0100

    Testing changes
    -Added sizing
    -Default middle of screen
    -Icon

--- Vang de Volger/Vang de Volger/Program.cs
+++ Vang de Volger/Vang de Volger/Program.cs
@@ -16,7 +16,7 @@ namespace Vang_de_Volger
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new Level());
         }
     }
 }
